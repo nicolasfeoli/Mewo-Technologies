bd7a479b03466623a778a57dcd797568f5a3a3de
OrlandoHR <orlandojosehr1997@gmail.com>
2019-10-27 04:58:32 -0600

    CU3 - Inicio 2

--- MiApp/app/src/main/java/com/exampleprueba/nicolasfeoli/miapp/ListaEstudiantes.java
+++ MiApp/app/src/main/java/com/exampleprueba/nicolasfeoli/miapp/ListaEstudiantes.java
@@ -1,13 +1,33 @@
 package com.exampleprueba.nicolasfeoli.miapp;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class ListaEstudiantes extends AppCompatActivity {
+public class ListaEstudiantes extends Activity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_estudiantes);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        String[] myDataset = {"",""};
+        // specify an adapter (see also next example)
+        mAdapter = new EstudiantesAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 }
